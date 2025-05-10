62d8d6310e55f6d9d9c7f99a617cbd38df8f0775
mdrubelhossain-me <mdrubelhossain.cse@gmail.com>
2025-05-10 20:50:49 +0600

    project created and setup complete

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-7xl text-primary font-bold'>Welcome to the Landing Page</h1>
+      <h1 className='text-7xl text-primary font-roboto font-bold'>Welcome to the Landing Page</h1>
+      <h1 className='text-7xl text-primary font-inter font-bold'>Welcome to the Landing Page</h1>
+      <h1 className='text-7xl text-primary font-poppins font-bold'>Welcome to the Landing Page</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1D4ED8',
+        secondary: '#9333EA',
+        accent: '#F59E0B',
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
